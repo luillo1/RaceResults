c02dd032d0ac540d8698812bbd22ea401a5a29f6
Carl Kadie <carlk@msn.com>
2021-11-18 14:52:43 -0800

    Add Delta methods to MemberMatch that can use a dictionary or TSV file that associates names with probabilities.

--- src/Libraries/MemberMatch/Scorer.cs
+++ src/Libraries/MemberMatch/Scorer.cs
@@ -1,13 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace MemberMatch
 {
-    static public class Scorer
+    public class Scorer
     {
         static public double defaultPriorScore = -11.51291546;
         static public double defaultProbabilityAppearsInLineFromReference = 0.6;
+        static private double defaultRare = 1e-5;
+
+        public Scorer(Dictionary<string, double> nameToProbability)
+        {
+            _nameToProbability = nameToProbability;
+        }
+
+
+        public Scorer(string nameToProbabilityFileName)
+        {
+            _nameToProbability = new Dictionary<string, double>();
+            int index = -1;
+            foreach (string line in File.ReadLines(nameToProbabilityFileName))
+            {
+                index++;
+                if (index==0)
+                {
+                    continue;
+                }
+                var fields = line.Split('\t');
+                _nameToProbability[fields[0]] = double.Parse(fields[1]);
+            }
+        }
+
+        private Dictionary<string, double> _nameToProbability;
 
         static public double Delta(
                                    double[] probabilityAppearsInLineByCoincidenceList,
@@ -37,6 +64,35 @@ namespace MemberMatch
             return delta;
         }
 
+        public double Delta(
+                                   string[] nameList,
+                                   bool[] isContainedList,
+                                   double[] probabilityAppearsInLineFromReferenceList = default)
+
+        {
+            var length = nameList.Length;
+            Trace.Assert(length == isContainedList.Length, "Expect lists to all have same length");
+            Trace.Assert(probabilityAppearsInLineFromReferenceList == default || length == probabilityAppearsInLineFromReferenceList.Length,
+                "Expect lists to all have same length");
+            if (length == 0)
+            {
+                return 0;
+            }
+
+            probabilityAppearsInLineFromReferenceList = _ReplaceAnyDefault(probabilityAppearsInLineFromReferenceList, length);
+
+            var delta = (from name in nameList
+                         from isContained in isContainedList
+                         from fromRef in probabilityAppearsInLineFromReferenceList
+                         let altScore = this.Delta(name: name,
+                             isContained: isContained,
+                             probabilityAppearsInLineFromReference: fromRef)
+                         select altScore)
+                        .Max();
+            return delta;
+        }
+
+
         private static double[] _ReplaceAnyDefault(double[] probabilityAppearsInLineFromReference, int length)
         {
             if (probabilityAppearsInLineFromReference == default)
@@ -55,6 +111,15 @@ namespace MemberMatch
 
             return probabilityAppearsInLineFromReference;
         }
+
+        public double Delta(string name,
+                                   bool isContained,
+                                   double probabilityAppearsInLineFromReference = double.NaN)
+        {
+            var probabilityAppearsInLineByCoincidence = this._nameToProbability.GetValueOrDefault(name, Scorer.defaultRare);
+            return Delta(probabilityAppearsInLineByCoincidence, isContained, probabilityAppearsInLineFromReference);
+        }
+
 
         static public double Delta(double probabilityAppearsInLineByCoincidence,
                                    bool isContained,
--- src/tests/TestMemberMatch/MemberMatchTests.cs
+++ src/tests/TestMemberMatch/MemberMatchTests.cs
@@ -1,5 +1,6 @@
 using MemberMatch;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -7,7 +8,20 @@ namespace TestMemberMatch
 {
     [TestClass]
     public class MemberMatchTests
-    {
+    { 
+    
+        private Scorer TestScorer()
+        {
+            var nameToProbability = new Dictionary<string, double>()
+            {
+                { "JOHN", .01 },
+                { "ALICE", 32.75 / 100_000},
+                { "SMITH",  823.92 / 100_000},
+                { "ALISON", 154.27 / 100_000},
+                { "ALLISON", 63.54 / 100_000}
+            };
+            return new Scorer(nameToProbability);
+        }
 
         [TestMethod]
         public void TestMethod0()
@@ -24,12 +38,14 @@ namespace TestMemberMatch
         [TestMethod]
         public void TestMethod1()
         {
+            var scorer = TestScorer();
+
             // We have a member named John and we see
             // "John" in a line of some results. 1% of Americans are
             // named "John". What is the score and
             // probability that this line refers to our member?
 
-            var score1 = Scorer.defaultPriorScore + Scorer.Delta(probabilityAppearsInLineByCoincidence: .01, isContained: true);
+            var score1 = Scorer.defaultPriorScore + scorer.Delta(name: "JOHN", isContained: true);
             Assert.AreEqual(score1, -7.4185, delta: .001);
             var probability1 = Scorer.ScoreToProbability(score1);
             Assert.AreEqual(probability1, 0.000599, delta: 0.0001);
@@ -39,7 +55,7 @@ namespace TestMemberMatch
             // Americans have that name. What is the score and
             // probability that this line refers to our member?
 
-            var score2 = Scorer.defaultPriorScore + Scorer.Delta(probabilityAppearsInLineByCoincidence: 32.75 / 100_000, isContained: true);
+            var score2 = Scorer.defaultPriorScore + scorer.Delta(name: "ALICE", isContained: true);
             Assert.AreEqual(score2, -3.9997, delta: .001);
             var probability2 = Scorer.ScoreToProbability(score2);
             Assert.AreEqual(probability2, 0.01799, delta: 0.0001);
@@ -54,8 +70,10 @@ namespace TestMemberMatch
             // 823.92 of 100,000 Americans have the name Smith
             // What is the score and probability that this line refers to our member?
 
-            var score1 = Scorer.defaultPriorScore + Scorer.Delta(probabilityAppearsInLineByCoincidence: 32.75 / 100_000, isContained: true);
-            var score2 = score1 + Scorer.Delta(probabilityAppearsInLineByCoincidence: 823.92 / 100_000, isContained: true);
+            var scorer = TestScorer();
+
+            var score1 = Scorer.defaultPriorScore + scorer.Delta(name: "ALICE", isContained: true);
+            var score2 = score1 + scorer.Delta(name: "SMITH", isContained: true);
             Assert.AreEqual(score2, 0.2883, delta: .001);
             var probability2 = Scorer.ScoreToProbability(score2);
             Assert.AreEqual(probability2, .572, delta: 0.01);
@@ -70,8 +88,9 @@ namespace TestMemberMatch
             // 823.92 of 100,000 Americans have the name Smith
             // What is the score and probability that this line refers to our member?
 
-            var score1 = Scorer.defaultPriorScore + Scorer.Delta(probabilityAppearsInLineByCoincidence: 32.75 / 100_000, isContained: true);
-            var score2 = score1 + Scorer.Delta(probabilityAppearsInLineByCoincidence: 823.92 / 100_000, isContained: false);
+            var scorer = TestScorer();
+            var score1 = Scorer.defaultPriorScore + scorer.Delta(name: "ALICE", isContained: true);
+            var score2 = score1 + scorer.Delta(name: "SMITH", isContained: false);
             Assert.AreEqual(score2, -4.9077, delta: .001);
             var probability2 = Scorer.ScoreToProbability(score2);
             Assert.AreEqual(probability2, 0.00738, delta: 0.0001);
@@ -88,9 +107,11 @@ namespace TestMemberMatch
             // 63.54  of 100,000 Americans have the name Allison
             // What is the score and probability that this line refers to our member?
 
-            var score0 = Scorer.Delta(probabilityAppearsInLineByCoincidence: 32.75 / 100_000, isContained: false, probabilityAppearsInLineFromReference: .5);
-            var score1 = Scorer.Delta(probabilityAppearsInLineByCoincidence: 154.27 / 100_000, isContained: true, probabilityAppearsInLineFromReference: .05);
-            var score2 = Scorer.Delta(probabilityAppearsInLineByCoincidence: 63.54 / 100_000, isContained: false, probabilityAppearsInLineFromReference: .05);
+            var scorer = TestScorer();
+
+            var score0 = scorer.Delta(name: "ALICE", isContained: false, probabilityAppearsInLineFromReference: .5);
+            var score1 = scorer.Delta(name: "ALISON", isContained: true, probabilityAppearsInLineFromReference: .05);
+            var score2 = scorer.Delta(name: "ALLISON", isContained: false, probabilityAppearsInLineFromReference: .05);
             var score012 = Scorer.defaultPriorScore + new[] { score0, score1, score2 }.Max();
             Assert.AreEqual(score012, -8.03442, delta: .001);
             var probability012 = Scorer.ScoreToProbability(score012);
@@ -99,7 +120,7 @@ namespace TestMemberMatch
             // We also see "Smith"
             // 823.92 of 100,000 Americans have the name Smith
             // What is the score and probability that this line refers to our member?
-            var score4 = score012 + Scorer.Delta(probabilityAppearsInLineByCoincidence: 823.92 / 100_000, isContained: true);
+            var score4 = score012 + scorer.Delta(name: "SMITH", isContained: true);
             Assert.AreEqual(score4, -3.74640, delta: .001);
             var probability4 = Scorer.ScoreToProbability(score4);
             Assert.AreEqual(probability4, 0.023058, delta: 0.0001);
@@ -109,12 +130,14 @@ namespace TestMemberMatch
         public void TestMethod5()
         {
             // We have a member named Alice from Bellevue.
-            // "Alice" in a line of some results. 32.75 of 100,000
-            // Americans have that name. What is the score and
+            // "Alice" in a line of some results.
+            // What is the score and
             // probability that this line refers to our member?
 
+            var scorer = TestScorer();
+
             var score = Scorer.defaultPriorScore;
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidence: 32.75 / 100_000, isContained: true);
+            score += scorer.Delta(name: "ALICE", isContained: true);
 
             // "Bellevue" appears in 30 of the 3000 result lines.
             var score1 = score + Scorer.Delta(probabilityAppearsInLineByCoincidence: (30.0 + 1.0) / (3000.0 + 2.0), isContained: true);
@@ -139,14 +162,15 @@ namespace TestMemberMatch
         [TestMethod]
         public void TestMethod6()
         {
-            // Let's put it all together.
             // We found a line containing "Alison" and we wonder if it refers to our member
             // Alice Smith, for whom "Alison" and "Allison" are nicknames.
             // The line does not contain "Smith", but it does contain "Bellevue" her town.
 
-            double[] firstNameCoincidenceList = new[] { 32.75 / 100_000, 154.27 / 100_000, 63.54 / 100_000 };
+            var scorer = TestScorer();
+
+            string[] firstNameList = new[] { "ALICE", "ALISON", "ALLISON" };
             bool[] firstNameIsContainedList = new[] { false, true, false };
-            double[] lastNameCoincidenceList = new[] { 823.9 / 100_000 };
+            string[] lastNameList = new[] { "SMITH" };
             bool[] lastNameIsContainedList = new[] { false };
             double[] cityConcidenceList = new[] { (300.0 + 1.0) / (3000.0 + 2.0) };
             bool[] cityIsContainedList = new[] { true };
@@ -155,11 +179,11 @@ namespace TestMemberMatch
             var score = Scorer.defaultPriorScore;
 
             // Evidence from First Name
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: firstNameCoincidenceList,
+            score += scorer.Delta(nameList: firstNameList,
                                  isContainedList: firstNameIsContainedList);
 
             // Evidence from Last Name
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: lastNameCoincidenceList,
+            score += scorer.Delta(nameList: lastNameList,
                                  isContainedList: lastNameIsContainedList);
 
             // Evidence from City
@@ -178,11 +202,13 @@ namespace TestMemberMatch
             // We found a line containing "Alice" and we wonder if it refers to our member
             // Alice Smith, for whom "Alison" and "Allison" are nicknames.
             // The line does contain "Smith", but it does not contain "Bellevue" her town
-            // even though there are city names in the results
+            // even though there are city names in the results.
 
-            double[] firstNameCoincidenceList = new[] { 32.75 / 100_000, 154.27 / 100_000, 63.54 / 100_000 };
+            var scorer = TestScorer();
+
+            string[] firstNameList = new[] { "ALICE", "ALISON", "ALLISON" };
             bool[] firstNameIsContainedList = new[] { true, false, false };
-            double[] lastNameCoincidenceList = new[] { 823.9 / 100_000 };
+            string[] lastNameList = new[] { "SMITH" };
             bool[] lastNameIsContainedList = new[] { true };
             double[] cityConcidenceList = new[] { (300.0 + 1.0) / (3000.0 + 2.0) };
             bool[] cityIsContainedList = new[] { false };
@@ -191,11 +217,11 @@ namespace TestMemberMatch
             var score = Scorer.defaultPriorScore;
 
             // Evidence from First Name
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: firstNameCoincidenceList,
+            score += scorer.Delta(nameList: firstNameList,
                                  isContainedList: firstNameIsContainedList);
 
             // Evidence from Last Name
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: lastNameCoincidenceList,
+            score += scorer.Delta(nameList: lastNameList,
                                  isContainedList: lastNameIsContainedList);
 
             // Evidence from City
@@ -216,20 +242,22 @@ namespace TestMemberMatch
             // The line does contain "Smith". None of the results contain city info, so we skip
             // that.
 
-            double[] firstNameCoincidenceList = new[] { 32.75 / 100_000, 154.27 / 100_000, 63.54 / 100_000 };
+            var scorer = TestScorer();
+
+            string[] firstNameList = new[] { "ALICE", "ALISON", "ALLISON" };
             bool[] firstNameIsContainedList = new[] { true, false, false };
-            double[] lastNameCoincidenceList = new[] { 823.9 / 100_000 };
+            string[] lastNameList = new[] { "SMITH" };
             bool[] lastNameIsContainedList = new[] { true };
 
             // Prior
             var score = Scorer.defaultPriorScore;
 
             // Evidence from First Name
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: firstNameCoincidenceList,
+            score += scorer.Delta(nameList: firstNameList,
                                  isContainedList: firstNameIsContainedList);
 
             // Evidence from Last Name
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: lastNameCoincidenceList,
+            score += scorer.Delta(nameList: lastNameList,
                                  isContainedList: lastNameIsContainedList);
 
 
@@ -246,28 +274,25 @@ namespace TestMemberMatch
             // and we wonder if it refers to our member Alice Smith,
             // for whom "Alison" and "Allison" are nicknames.
             // The line also contains "Smith" and "Bellevue", her town.
-            // From outside sources we know:
-            // 32.75 of 100,000 Americans have the name Alice
-            // 154.27 of 100,000 Americans have the name Alison
-            // 63.54  of 100,000 Americans have the name Allison
-            // 823.92 of 100,000 Americans have the name Smith
             // We can also see that 300 of the 3000 result lines mention "Bellevue".
 
-            // For each first name and related nicknames, we list the probability
+            var scorer = TestScorer();
+
+            // We list each first name and related nicknames.
+            // Behind the scenes it looks up the probability
             // that we might see this name on an output line even if this line
             // doesn't refer to our Alice Smith.
-            double[] firstNameCoincidenceList = new[] { 32.75 / 100_000, 154.27 / 100_000, 63.54 / 100_000 };
+            string[] firstNameList = new[] { "ALICE", "ALISON", "ALLISON" };
             // We also tell which of her first names (if any) we saw.
             bool[] firstNameIsContainedList = new[] { true, false, false };
 
             // We provide the same info for her last name(s)
-            double[] lastNameCoincidenceList = new[] { 823.9 / 100_000 };
+            string[] lastNameList = new[] { "SMITH" };
             // We found "Smith" on the line, so this is 'true'
             bool[] lastNameIsContainedList = new[] { true };
 
             // Finally, if the race results include city information, we include
-            // the probability of seeing her city on this result line in the
-            // case where this line is not about her.
+            // the probability of seeing her city on the result line of someone else.
             double[] cityConcidenceList = new[] { (300.0 + 1.0) / (3000.0 + 2.0) };
             // We found "Bellevue" on the line, so this is 'true'
             bool[] cityIsContainedList = new[] { true };
@@ -277,19 +302,19 @@ namespace TestMemberMatch
             var score = Scorer.defaultPriorScore;
             Debug.WriteLine($"Score {score} -- probability {Scorer.ScoreToProbability(score):P3}");
 
-            // Seeing "Alice" on the line moves the score up 7 to -4.
+            // Seeing "Alice" on the line moves the score up by 8 to -4.
             // We now think there is a 1.5% chance this line refers to our Alice.
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: firstNameCoincidenceList,
+            score += scorer.Delta(nameList: firstNameList,
                                  isContainedList: firstNameIsContainedList);
             Debug.WriteLine($"Score {score} -- probability {Scorer.ScoreToProbability(score):P3}");
 
-            // Seeing "Smith" moves the score up 4 to 0.
-            // We now think there is a 50% chance this line refers to our Alice.
-            score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: lastNameCoincidenceList,
+            // Seeing "Smith" moves the score up by 4 to 0.
+            // We now think there is a 52% chance this line refers to our Alice.
+            score += scorer.Delta(nameList: lastNameList,
                                  isContainedList: lastNameIsContainedList);
             Debug.WriteLine($"Score {score} -- probability {Scorer.ScoreToProbability(score):P3}");
 
-            // Seeing "Bellevue" move the score from 0 to 2.
+            // Seeing "Bellevue" moves the score from 0 to 2.
             // We now think there is an 87% chance this line refers to our Alice.
             score += Scorer.Delta(probabilityAppearsInLineByCoincidenceList: cityConcidenceList,
                                  isContainedList: cityIsContainedList);
@@ -298,7 +323,7 @@ namespace TestMemberMatch
             // Check Results
             Assert.AreEqual(score, 1.89510, delta: .001);
             var probability = Scorer.ScoreToProbability(score);
-            Assert.AreEqual(probability,  0.8693, delta: 0.0001);
+            Assert.AreEqual(probability, 0.8693, delta: 0.0001);
         }
 
     }
