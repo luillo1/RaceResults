ffa808ad7c706c82314e2140a90db03c1f504cdf
Luke Bordonaro <lukebordonaro@gmail.com>
2021-11-26 16:30:19 -0800

    Frontend skeleton (#12)
    * Init react app
    
    * Add eslint
    
    * Ignore frontend folder in csproj
    
    * WIP
    
    * Skeleton for frontend finished
    
    * Working navbar and routing
    
    * Rename frontend to client
    
    * undo changes to csproj
    
    * Update eslint package dependencies
    
    * Remove unused file
    
    * Added workflow for client deployment
    
    Co-authored-by: MutatedGamer <mutatedgamerxl@gmail.com>
    Co-authored-by: Luis Torres <luillo1@gmail.com>

---
+++ client/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  if (mode === "development") {
+    return {
+      plugins: [react()],
+      server: {
+        proxy: {
+          "/api": {
+            target: "https://localhost:5001",
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/api/, "")
+          }
+        }
+      }
+    };
+  } else {
+    return {
+      plugins: [react()]
+    };
+  }
+});
