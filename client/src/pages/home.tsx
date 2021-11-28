44dee9ec2509bc8e0930f6754e81ef750b70506f
MutatedGamer <mutatedgamerxl@gmail.com>
2021-11-28 12:17:27 -0800

    Add staticwebapp config

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,8 @@
+import React from "react";
 import { faRunning } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 import {
   Segment,
-  Dimmer,
-  Loader,
-  Placeholder,
   Container,
   Header,
   Icon,
