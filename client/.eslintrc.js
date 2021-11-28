b264656df29344f1ba6920f1931a1b359f71dcac
MutatedGamer <mutatedgamerxl@gmail.com>
2021-11-28 15:36:01 -0800

    Add lint check and CI workflow

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     commonjs: true,
     es2021: true
   },
-  extends: ["plugin:react/recommended", "standard"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard",
+    "plugin:@typescript-eslint/recommended"
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -14,10 +20,16 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
+    "react/jsx-uses-react": 1,
     quotes: [2, "double"],
     "space-before-function-paren": ["error", "never"],
     semi: ["warn", "always"],
     "no-use-before-define": "off",
     "@typescript-eslint/no-use-before-define": ["error"]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
 };
