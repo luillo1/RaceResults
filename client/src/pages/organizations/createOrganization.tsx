3b139efd6c0987318918b16c3e1044d5560df1e1
MutatedGamer <mutatedgamerxl@gmail.com>
2021-12-03 18:02:09 -0800

    Add page for creating org

---
+++ client/src/pages/organizations/createOrganization.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Header, Segment } from "semantic-ui-react";
+
+const CreateOrganizationPage = () => (
+  <Segment vertical>
+    <Container>
+      <Header as="h2" content="Create Organization" />
+    </Container>
+  </Segment>
+);
+
+export default CreateOrganizationPage;
