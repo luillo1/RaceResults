22680daa03bd915a2463499a1952721ba09f066f
Luke Bordonaro <lukebordonaro@gmail.com>
2021-12-26 21:12:19 -0500

    Add vite-plugin-commonjs plugin (#59)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,28 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   if (mode === "development") {
     return {
-      plugins: [react()],
+      plugins: [react(), viteCommonjs()],
       server: {
         proxy: {
           "/api": {
             target: "https://localhost:5001",
             changeOrigin: true,
             secure: false,
-            rewrite: (path) => path.replace(/^\/api/, "")
-          }
-        }
-      }
+            rewrite: (path) => path.replace(/^\/api/, ""),
+          },
+        },
+      },
     };
   } else {
     return {
       plugins: [react()],
       build: {
-        outDir: "./build"
-      }
+        outDir: "./build",
+      },
     };
   }
 });
